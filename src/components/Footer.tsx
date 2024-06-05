bad54576a820d47784825324cb708fccac9870c5
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-06-05 17:16:25 +0200

    📝 (Footer.tsx): remove redundant class 'mt-0' to improve code readability and maintainability

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 	return (
 		<footer className="bg-white mt-auto">
 			<div className="mx-auto max-w-7xl px-6 py-12 flex items-center justify-center lg:px-8">
-				<div className="mt-8 order-1 mt-0">
+				<div className="mt-8 order-1">
 					<p className="text-center text-xs leading-5 text-gray-500 flex items-center">
 						&copy; {new Date().getFullYear()}, fait avec{" "}
 						<HeartIcon className="h-4 w-4 mx-1" /> Anthony Gorski.
